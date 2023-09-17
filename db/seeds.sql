f54d48ce115aadb4591df6aa286a041b9dd3fb94
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-09-17 16:06:36 -0400

    added sql connection and prompt in .js file

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,12 +3,16 @@ VALUES ("Sales"),
        ("Engineering"),
        ("Service Delivery");
 
-/*INSERT INTO role (name, salary, )
-VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
-       (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
-       (1, "Scar is the lion everyone loves to hate"),
-       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
-       (5, "The tin man gave a metallic, hollow performance"),
-       (1, "Hakuna matata"),
-       (5, "Those flying monkeys are nightmare fuel!");*/
+INSERT INTO role (name, salary, department_id)
+VALUES ("Sales consultant", 100, 1),
+       ("Software Engineer", 200, 2),
+       ("Application Engineer", 150, 3);       
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Neha","Sab", 2, NULL ),
+       ("Maria", "Ahmed", 3, 1),
+       ("Roxana","Fig", 1, 1); 
+
+
        
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-
+const inquirer = require("inquirer");
 const password = process.env.PASSWORD;
 
 // Import and require mysql2
 const mysql = require('mysql2');
 
 // Connect to database
-const db = mysql.createConnection(
+const connection = mysql.createConnection(
     {
       host: 'localhost',
       // MySQL username,
@@ -15,132 +15,35 @@ const db = mysql.createConnection(
       password: `${password}`,
       database: 'company_db'
     },
-    console.log(`Connected to the company_db database.`)
   );
 
-/*const express = require('express');
-// Import and require mysql2
-const mysql = require('mysql2');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // TODO: Add MySQL password here
-    password: '',
-    database: 'movies_db'
-  },
-  console.log(`Connected to the movies_db database.`)
-);
-
-// Create a movie
-app.post('/api/new-movie', ({ body }, res) => {
-  const sql = `INSERT INTO movies (movie_name)
-    VALUES (?)`;
-  const params = [body.movie_name];
-  
-  db.query(sql, params, (err, result) => {
+  //check the connection to database
+  connection.connect((err)=> {
     if (err) {
-      res.status(400).json({ error: err.message });
+      console.error('Error connecting to the database: ',err);
       return;
     }
-    res.json({
-      message: 'success',
-      data: body
-    });
-  });
-});
+    
+    console.log("Connected to company_db");
+    questions();
+    
+  })
 
-// Read all movies
-app.get('/api/movies', (req, res) => {
-  const sql = `SELECT id, movie_name AS title FROM movies`;
-  
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-       return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
-});
+function questions() {
+  inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Exit",
+            ],
+        })
+        .then((answer) => {
+            console.log(answer)
+        });
+}
 
-// Delete a movie
-app.delete('/api/movie/:id', (req, res) => {
-  const sql = `DELETE FROM movies WHERE id = ?`;
-  const params = [req.params.id];
-  
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.statusMessage(400).json({ error: res.message });
-    } else if (!result.affectedRows) {
-      res.json({
-      message: 'Movie not found'
-      });
-    } else {
-      res.json({
-        message: 'deleted',
-        changes: result.affectedRows,
-        id: req.params.id
-      });
-    }
-  });
-});
-
-// Read list of all reviews and associated movie name using INNER JOIN
-app.get('/api/movie-reviews', (req, res) => {
-  const sql = `SELECT movies.movie_name AS movie, reviews.review FROM reviews INNER JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
-});
-
-// BONUS: Update review name
-app.put('/api/review/:id', (req, res) => {
-  const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
-  const params = [req.body.review, req.params.id];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-    } else if (!result.affectedRows) {
-      res.json({
-        message: 'Movie not found'
-      });
-    } else {
-      res.json({
-        message: 'success',
-        data: req.body,
-        changes: result.affectedRows
-      });
-    }
-  });
-});
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-*/
